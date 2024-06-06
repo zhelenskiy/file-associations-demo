00a81e9f765921912a291d85ea307526deb9270d
Evgeniy.Zhelenskiy <zhelenskiy2000@yandex.ru>
2024-06-06 12:19:43 +0200

    Add a simple desktop app with file associations

---
+++ settings.gradle.kts
@@ -0,0 +1,16 @@
+pluginManagement {
+    repositories {
+        mavenLocal()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+
+    plugins {
+        kotlin("jvm").version(extra["kotlin.version"] as String)
+        id("org.jetbrains.compose").version(extra["compose.version"] as String)
+    }
+}
+
+rootProject.name = "file-associations-demo"
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,38 @@
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+import java.awt.Desktop
+import javax.swing.JOptionPane
+
+@Composable
+@Preview
+fun App() {
+    var text by remember { mutableStateOf("Hello, World!") }
+
+    MaterialTheme {
+        Button(onClick = {
+            text = "Hello, Desktop!"
+        }) {
+            Text(text)
+        }
+    }
+}
+
+fun main() {
+    Desktop.getDesktop().setOpenFileHandler { event ->
+        JOptionPane.showMessageDialog(null, event.files.map { it.path })
+    }
+    application {
+        Window(onCloseRequest = ::exitApplication) {
+            App()
+        }
+    }
+}
